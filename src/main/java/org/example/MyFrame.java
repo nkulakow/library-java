b1258999c98271cff3894ad473c501edcac1c3d7
Marcin <01169256@pw.edu.pl>
2022-11-22 15:02:35 +0100

    funn example

--- src/main/java/org/example/MyFrame.java
+++ src/main/java/org/example/MyFrame.java
@@ -5,26 +5,32 @@ import java.awt.*;
 
 public class MyFrame extends JFrame{
     String frameName;
-    MyFrame()
+    int width;
+    int height;
+    MyFrame(final int f_width, final int f_height)
     {
         this.frameName = "My beautiful frame";
+        this.width = f_width;
+        this.height = f_height;
         this.setAttributes();
         this.setVisible(true); //makes frame visible
         this.getIcon();
     }
 
-    void setAttributes()
+    private void setAttributes()
     {
-        this.setSize(420, 420);
+        this.setSize(this.width, this.height);
         this.setTitle(this.frameName);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //stops setVisible command - exit out application
-        this.setResizable(false);
+        this.setResizable(true);
         this.setLocation(700,200);
         this.getContentPane().setBackground(new Color(204, 153, 255));
     }
-    void getIcon()
+    private void getIcon()
     {
         ImageIcon image = new ImageIcon("src/20220715_052950.jpg");
         this.setIconImage(image.getImage());
     }
+
+
 }
