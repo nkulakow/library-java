115232c1de9ec37aca833b595d4ce7783f6913ee
Marcin <01169256@pw.edu.pl>
2022-12-04 12:52:46 +0100

    preparation before merging with library context

--- src/main/java/org/example/GUI/LogInPage.java
+++ src/main/java/org/example/GUI/LogInPage.java
@@ -14,14 +14,12 @@ class LogInPage extends Page {
     private JTextField login_field;
     private JPasswordField password_field;
     private JButton login_button;
-    private JLabel login_text;
-    private JLabel password_text;
     private JLabel prompt;
 
     private static final String LOGIN = "root";
     private static final char[] PASSWORD = new char[] {'r', 'o', 'o', 't'};
 
-    private JPanel panel;
+    private final JPanel panel;
 
     public LogInPage() {
         super(LogInPage.width, LogInPage.height);
@@ -38,10 +36,10 @@ class LogInPage extends Page {
         this.password_field.setBounds(300, 300, 200, 30);
         this.login_button = new JButton("Log in");
         this.login_button.setBounds(300, 350, 80, 40);
-        this.login_text = new JLabel("Enter login");
-        this.login_text.setBounds(300, 160, 100, 30);
-        this.password_text = new JLabel("Enter password");
-        this.password_text.setBounds(300, 260, 150, 30);
+        JLabel login_text = new JLabel("Enter login");
+        login_text.setBounds(300, 160, 100, 30);
+        JLabel password_text = new JLabel("Enter password");
+        password_text.setBounds(300, 260, 150, 30);
         this.prompt = new JLabel("Enter Your login and password.");
         this.prompt.setBounds(150, 420, 500, 40);
         this.prompt.setFont(new Font(this.prompt.getFont().getName(), Font.PLAIN, 20));
@@ -49,8 +47,8 @@ class LogInPage extends Page {
         this.panel.add(this.login_field);
         this.panel.add(this.password_field);
         this.panel.add(this.login_button);
-        this.panel.add(this.login_text);
-        this.panel.add(this.password_text);
+        this.panel.add(login_text);
+        this.panel.add(password_text);
         this.panel.setBackground(new Color(204, 153, 255));
         var bottomPanel = new JPanel();
         bottomPanel.setBounds(0, 500, 800, 100);
@@ -68,12 +66,14 @@ class LogInPage extends Page {
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        System.out.println("bly");
         if (e.getSource() == this.login_button) {
             if(this.checkLogging()) {
+                this.prompt.setText("Logged in");
                 LibraryGUI.changeAfterLogged();
             }
             else {
-                this.prompt.setText("Incorrect data. Try again.");
+                this.prompt.setText("Incorrect login and/or password");
             }
         }
     }
--- src/main/java/org/example/GUI/MainPage.java
+++ src/main/java/org/example/GUI/MainPage.java
@@ -1,12 +1,14 @@
 package org.example.GUI;
 
 import javax.swing.*;
+import javax.swing.border.LineBorder;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.util.List;
 import java.util.Vector;
 
 public class MainPage extends Page {
-    private JPanel options;
+    private OptionPanel options;
     public MainPage () {
         super(Toolkit.getDefaultToolkit().getScreenSize().width, Toolkit.getDefaultToolkit().getScreenSize().height);
         this.init();
@@ -25,7 +27,10 @@ public class MainPage extends Page {
                 "Modify database",
                 "Show my account",
         };
-        this.options = new OptionPanel(new Vector<String>(List.of(option_text)));
+        this.options = new OptionPanel(new Vector<>(List.of(option_text)));
+        for (var option : this.options.getOptions()) {
+            option.addActionListener(this);
+        }
         this.add(options, BorderLayout.WEST);
     }
 
@@ -36,34 +41,88 @@ public class MainPage extends Page {
     private void initCenter() {
         this.add(new CenterPannel(), BorderLayout.CENTER);
     }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource() instanceof OptionPanel.OptionButton button) {
+            switch (button.getActionType()) {
+                case OptionPanel.OptionButton.SEARCH_IN_DATABASE:
+
+                    break;
+                case OptionPanel.OptionButton.MODIFY_DATABASE:
+
+                    break;
+                case OptionPanel.OptionButton.SHOW_ACCOUNT:
+
+                default:
+                    break;
+            }
+        }
+    }
 }
 
 class OptionPanel extends JPanel {
-    private Vector<String> options_text;
-    private Vector<JButton> options;
+    final private Vector<String> options_text;
+    private Vector<OptionButton> options;
 
     public OptionPanel(final Vector<String> foptions) {
         this.options_text = foptions;
-        this.options = new Vector<JButton>();
+        this.setLayout(null);
+        this.setBackground(Color.ORANGE);
+        this.setPreferredSize(new Dimension(250, 400));
+        this.initOptions();
+    }
 
+    private void initOptions() {
+        this.options = new Vector<>();
+        int i = 0;
         for(String option : this.options_text) {
-            var button = new JButton(option);
-            button.setBackground(new Color(179, 122, 82));
-            button.setForeground(new Color(60,60 ,60));
-            button.setFont(new Font(Font.SERIF, Font.ITALIC, 30));
-            button.setFocusPainted(false);
-            button.setHorizontalAlignment(JButton.CENTER);
+            OptionButton button = new OptionButton(option);
+            button.setBounds(0, 55 * i, 250, 50);
             this.options.add(button);
             this.add(button);
+            i++;
         }
-        this.setLayout(new GridLayout(options.size(), 1));
+        this.options.get(0).setActionType(OptionButton.SEARCH_IN_DATABASE);
+        this.options.get(1).setActionType(OptionButton.MODIFY_DATABASE);
+        this.options.get(2).setActionType(OptionButton.SHOW_ACCOUNT);
+    }
+
+    public Vector<OptionButton> getOptions() {
+        return this.options;
+    }
+
+    static class OptionButton extends JButton {
+        private int action;
+
+        public final static int MODIFY_DATABASE = 0;
+        public final static int SEARCH_IN_DATABASE = 1;
+        public final static int SHOW_ACCOUNT = 2;
+
+        OptionButton(final String text) {
+            super(text);
+            this.setBackground(new Color(179, 122, 82));
+            this.setForeground(new Color(60,60 ,60));
+            this.setFont(new Font(Font.SERIF, Font.ITALIC, 20));
+            this.setFocusPainted(false);
+            this.setHorizontalAlignment(JButton.CENTER);
+            this.setBorder(new LineBorder(Color.BLACK));
+        }
+
+        public int getActionType() {
+            return this.action;
+        }
+
+        public void setActionType(final int new_action) {
+            this.action = new_action;
+        }
     }
 }
 
 class BottomPanel extends JPanel {
     public BottomPanel() {
         this.setBackground(new Color(94, 94, 94));
-        this.setPreferredSize(new Dimension(100, 200));
+        this.setPreferredSize(new Dimension(100, 100));
     }
 }
 
