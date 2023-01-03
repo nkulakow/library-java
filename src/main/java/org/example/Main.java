eda13c548c62f305112bb3910c530c1211866a59
Nel <01169201@pw.edu.pl>
2023-01-03 18:01:39 +0100

    added few logs (still draft) and 1 unit test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,10 @@
 package org.example;
 import org.example.GUI.LibraryGUI;
-import org.example.LibraryContextPackage.InvalidBookNumberException;
-import org.example.LibraryContextPackage.InvalidIdException;
 import org.example.LibraryContextPackage.LibraryContext;
-import org.example.LibraryContextPackage.NullOrEmptyStringException;
 
 public class Main {
     //  static LibraryGUI gui_manager;
-    public static void main(String[] args) throws NullOrEmptyStringException, InvalidIdException, InvalidBookNumberException {
+    public static void main(String[] args) {
 
 
         LibraryGUI.GUIInit();
